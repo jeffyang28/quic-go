429093a678af8e614e4815e767a5a82e19e3835e
Marten Seemann <martenseemann@gmail.com>
2017-08-23 10:24:25 +0700

    run the integration tests with race detector on Travis

--- .travis/script.sh
+++ .travis/script.sh
@@ -10,11 +10,13 @@ fi
 if [ ${TESTMODE} == "integration" ]; then
   # run benchmark tests
   ginkgo -randomizeAllSpecs -randomizeSuites -trace benchmark -- -samples=1
-  # run benchmark tests with the Go race detector
-  # The Go race detector only works on amd64.
-  if [ ${TRAVIS_GOARCH} == 'amd64' ]; then
-    ginkgo -race -randomizeAllSpecs -randomizeSuites -trace benchmark -- -samples=1 -size=10
-  fi
   # run integration tests
   ginkgo -r -v -randomizeAllSpecs -randomizeSuites -trace integrationtests
+fi
+
+if [ ${TESTMODE} == "race_integration" ]; then  
+  # run benchmark tests
+  ginkgo -race -randomizeAllSpecs -randomizeSuites -trace benchmark -- -samples=1 -size=10
+  # run integration tests
+  ginkgo -race -v -r -randomizeAllSpecs -randomizeSuites -trace integrationtests
 fi
